9b51d091b1016988e149202d9bc27201d4c00da3
Ebru Acikel <acikel.ebru@gmail.com>
2012-11-12 18:14:06 +0700

    devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ArkavisHp::Application.routes.draw do
+  devise_for :admins
+
   resources :games,       :only => [:show]
   resources :jobs,        :only => [:show]
   resources :categories,  :only => [:show]
