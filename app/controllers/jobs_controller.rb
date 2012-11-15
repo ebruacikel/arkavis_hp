5af1efc7dc02d1a1f20ef20dcdb476b494045422
Ebru Acikel <acikel.ebru@gmail.com>
2012-11-15 16:32:49 +0700

    pagination added to admin/pages and jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.all
+    @jobs = Job.order("id DESC").page(params[:page]).per(10)
 
     respond_to do |format|
       format.html # index.html.erb
